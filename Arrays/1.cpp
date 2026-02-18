7383a0ac3988196c87fbd7913e625a446a8202a6
Ayush <ayushsingla206@gmail.com>
2026-02-18 22:24:07 +0530

    Questions for 18/02/26

---
+++ Arrays/1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+int method1 () {
+	string s, str2;
+	cin >> s;
+
+    for (int i=s.size()-1; i>=0; i--) {
+	    str2 += s[i];
+    }
+
+	cout << str2;
+}
+*/
+
+/*
+int method2 () {
+	string s;
+	cin >> s;
+	
+	char temp;
+	
+	for (int i=0; i<s.size()/2; i++) {
+		temp = s[i];
+		s[i]=s[s.size()-1-i];
+		s[s.size()-1-i]=temp;
+	}
+
+	cout << s;
+}
+*/
+
+int main() {
+	
+}
